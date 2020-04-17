7f0f6307a2f85f648b51f8b0abbce99c8ef0bd87
FJX_SJ <fengjx@lysjkj.net>
2020-04-17 19:07:17 +0800

    RouterHook1.0版

--- routerhook/config.json.js
+++ routerhook/config.json.js
@@ -1,9 +1,9 @@
 {
-"version":"0.2",
-"md5":"a7db99d68bf6a555ed71719170fff246",
-"home_url":"Module_routerhook.asp",
-"title":"RouterHook事件回调",
-"description":"从路由器推送状态及通知的工具",
-"changelog":"",
-"build_date":"2020-04-03_23:08:10"
+    "version": "1.0",
+    "md5": "",
+    "home_url": "Module_routerhook.asp",
+    "title": "RouterHook事件回调",
+    "description": "从路由器推送状态及通知的工具",
+    "changelog": "",
+    "build_date": "2020-04-03_23:08:10"
 }
