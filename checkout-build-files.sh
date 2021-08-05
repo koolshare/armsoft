a9b28bcd7563585e841e4b26605c1b00b18214c6
sadoneli <sadoneli@gmail.com>
2021-08-05 19:53:16 +0800

    modify bash files

--- checkout-build-files.sh
+++ checkout-build-files.sh
@@ -1,3 +1,5 @@
- git status|grep tar.gz|cut -d ' ' -f4|xargs git checkout
- git status|grep version|cut -d ' ' -f4|xargs git checkout
- git status|grep config.json.js|cut -d ' ' -f4|xargs git checkout
+#!/bin/bash
+
+git status|grep tar.gz|cut -d ' ' -f4|xargs git checkout
+git status|grep version|cut -d ' ' -f4|xargs git checkout
+git status|grep config.json.js|cut -d ' ' -f4|xargs git checkout
--- softcenter/build_all.sh
+++ softcenter/build_all.sh
@@ -2,21 +2,20 @@
 
 #CURR_PATH=$(pwd)
 CURR_PATH="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
-cd ${CURR_PATH}
+SOFT_PATH=$(dirname $CURR_PATH)
+cd ${SOFT_PATH}
 
 modules=$(find . -maxdepth 1 -type d | grep -iv "\.\/\."|sed 's/.\///g'|sed '/\./d')
 for module in ${modules}; do
 	#echo ${module}
-	if [ -f ${CURR_PATH}/${module}/build.sh ]; then
-		echo "add .valid to ${module}"
-		cd ${CURR_PATH}/${CURR_PATH}
-		echo hnd > .valid
-		cd ../..
+	if [ -f ${SOFT_PATH}/${module}/build.sh ]; then
+		cd ${SOFT_PATH}/${module}
 		
+		echo "add .valid to ${module}"
+		echo arm384 >${SOFT_PATH}/${module}/${module}/.valid
+
 		echo "build ${module}"
-		cd ${CURR_PATH}/${module}
 		sh build.sh
-		cd ..
 	else
 		echo "${module}: this module do not have build.sh script!"
 	fi
