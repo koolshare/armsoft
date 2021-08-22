e1627a6b3d1825844dfaad3054a75f7837559fbd
sadoneli <sadoneli@gmail.com>
2021-08-22 16:10:17 +0800

    update

--- kms/config.json.js
+++ kms/config.json.js
@@ -1,12 +1,12 @@
 {
 "version":"1.5",
-"md5":"febc8d2bd16621c0784c63cd4e105bf4",
+"md5":"3c778b48a33b50e8ad2b37524008b572",
 "home_url":"Module_kms.asp",
 "title":"系统工具",
 "description":"kms",
 "tags":"辅助 工具",
-"author":"",
+"author":"anonymous",
 "link":"",
 "changelog":"",
-"build_date":"2021-08-05_20:49:34"
+"build_date":"2021-08-22_16:10:09"
 }
