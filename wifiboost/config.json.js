16788bac504c0745546044636ad170c8728628c5
sadog <sadoneli@gmail.com>
2021-11-24 21:41:53 +0800

    update

--- wifiboost/config.json.js
+++ wifiboost/config.json.js
@@ -1,6 +1,6 @@
 {
-"version":"3.6",
-"md5":"783546774e77115a5872d20738a9bbe1",
+"version":"3.7",
+"md5":"2117a46d68fe66cdc0016c42323ada9e",
 "home_url":"Module_wifiboost.asp",
 "title":"wifi boost arm",
 "description":"wifi boost: 路由器WiFi功率增强，增加信号覆盖范围。",
@@ -8,7 +8,7 @@
 "tags":"系统 工具",
 "author":"sadog",
 "link":"",
-"build_date":"2021-11-24_20:20:32",
+"build_date":"2021-11-24_21:18:45",
 "server":"42.192.18.234",
 "port":"8083"
 }
