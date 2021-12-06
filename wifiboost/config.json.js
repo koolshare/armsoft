1f4a2229b34ec7d8fc75b72559133812e9c6153b
sadog <sadoneli@gmail.com>
2021-12-06 16:04:39 +0800

    update wifiboost v3.9

--- wifiboost/config.json.js
+++ wifiboost/config.json.js
@@ -1,6 +1,6 @@
 {
-"version":"3.8",
-"md5":"3fb32b87db5f953e01c41e2424b66a9d",
+"version":"3.9",
+"md5":"2fc34858962f6847bf3bebaa531574e4",
 "home_url":"Module_wifiboost.asp",
 "title":"wifi boost arm",
 "description":"wifi boost: 路由器WiFi功率增强，增加信号覆盖范围。",
@@ -8,7 +8,7 @@
 "tags":"系统 工具",
 "author":"sadog",
 "link":"",
-"build_date":"2021-12-06_16:00:11",
+"build_date":"2021-12-06_16:04:28",
 "server":"42.192.18.234",
 "port":"8083"
 }
