533c4f5f35ec6d569c73c03c333448128aae30d3
Curry <46918772+CurryEx@users.noreply.github.com>
2022-01-16 20:40:10 +0800

    为CloudFlareDDNS加入定时更新的功能

--- cfddns/cfddns/uninstall.sh
+++ cfddns/cfddns/uninstall.sh
@@ -6,3 +6,8 @@ rm -rf /koolshare/res/icon-cfddns.png
 rm -rf /koolshare/scripts/cfddns*.sh
 rm -rf /koolshare/scripts/uninstall_cfddns.sh
 rm -rf /koolshare/webs/Module_cfddns.asp
+
+# 删除crontab中的配置
+username=`nvram show|grep http_username|awk -F '=' '{print $2}'`
+filename=/tmp/var/spool/cron/crontabs/$username
+sed -i '/S99cfddns.sh/d' $filename
