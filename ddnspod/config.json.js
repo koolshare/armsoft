1a02aa0d1406344682fea1e0fb4688e4fa1121e5
Joshua Su <1787548456@qq.com>
2023-01-19 10:50:38 +0800

    添加ipv6支持

--- ddnspod/config.json.js
+++ ddnspod/config.json.js
@@ -1,6 +1,6 @@
 {
-"version":"1.5",
-"md5":"9784d8c4dcbeba110a7da2db06ff3bd6",
+"version":"1.6",
+"md5":"d3377bf6422f3340fbe2a66400575d15",
 "home_url":"Module_ddnspod.asp",
 "title":"ddnspod",
 "description":"ddnspod",
@@ -8,5 +8,5 @@
 "author":"freexiaoyao",
 "link":"",
 "changelog":"",
-"build_date":"2021-08-05_20:49:36"
+"build_date":"2023-01-19_10:45:36"
 }
