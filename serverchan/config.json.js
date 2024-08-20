79cf5fb9489a3f226c22b8f531f74c03fc951451
Easy <easychen@qq.com>
2024-08-20 15:56:44 +0800

    更新server酱说明文案

--- serverchan/config.json.js
+++ serverchan/config.json.js
@@ -1,6 +1,6 @@
 {
 "version":"1.4.1",
-"md5":"557342bf0bd041e23660d2eec2c1ad55",
+"md5":"0220164c004123c0aa0729539af9a3c5",
 "home_url":"Module_serverchan.asp",
 "title":"ServerChan微信推送",
 "description":"从路由器推送状态及通知的工具",
@@ -8,5 +8,5 @@
 "author":"clang",
 "link":"",
 "changelog":"",
-"build_date":"2024-08-20_15:39:20"
+"build_date":"2024-08-20_15:56:17"
 }
